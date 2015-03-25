9a0ae2a7a372b0a4bdebd40270c15152886cc3b3
psaindon <bitbucket@psaindon.com>
2015-03-25 16:18:10 -0400

    Fix problem where paths that start the same as home are being replaced by a ~

--- plugins/c9.core/util.js
+++ plugins/c9.core/util.js
@@ -267,11 +267,10 @@ define(function(require, exports, module) {
                 + md5Email + "?s=" + size + "&d="  + (defaultImage || "retro");
         };
         
-        var reHome = new RegExp("^" + plugin.escapeRegExp(c9.home || "/home/ubuntu"));
+        var reHome = new RegExp("^" + plugin.escapeRegExp(c9.home || "/home/ubuntu") + "/");
         plugin.normalizePath = function(path){
             if (!path || path.charAt(0) == "~") return path;
-            if (c9.home == "/") return ("/" + path.replace(/^[\/]+/, ""));
-            return (path.replace(/^[\/]+/, "/")).replace(reHome, "~");
+            return (path.replace(/^[\/]+/, "/")).replace(reHome, "~/");
         };
         
         /**
